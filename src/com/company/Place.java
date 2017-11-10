b4fe3db18ac6124e410d9057abe47f5aea0cf08d
JonathanPetit <petit.jonathan16@gmail.com>
2017-11-10 16:07:06 +0100

    Class place

--- src/com/company/Place.java
+++ src/com/company/Place.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Place {
+    private Size size;
+    private Vehicle vehicle;
+    private boolean free;
+
+    public Place(Size size) {
+        this.vehicle = null;
+        this.size = size;
+        this.free = true;
+    }
+
+    public void setBusy(Vehicle vehicle) {
+        this.free = false;
+        this.vehicle = vehicle;
+    }
 }
---
+++ src/com/company/Size.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Size {
+    private int length;
+    private int width;
+
+    public Size(int width, int length) {
+        this.width = width;
+        this.length = length;
+    }
+}
