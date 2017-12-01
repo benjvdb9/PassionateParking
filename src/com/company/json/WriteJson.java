9d009c17f6a943499db0afef1b894cfa9e84bcc3
manoelledeviron <manoelle.deviron@skynet.be>
2017-12-01 16:05:34 +0100

    json write working

--- src/com/company/json/WriteJson.java
+++ src/com/company/json/WriteJson.java
@@ -13,9 +13,17 @@ import javax.json.stream.JsonGenerator;
 public class WriteJson {
     public static void main(String[] args) throws IOException {
         OutputStream fileOutputStream = new FileOutputStream("Parking.json");
+        JsonGenerator generator = Json.createGenerator(fileOutputStream);
 
         Size newSize = new Size(200,300);
         Place newPlace = new Place(newSize);
+        generator.writeStartObject();
+        generator.write("length",newPlace.getSize_place().getLength())
+                .write("width",newPlace.getSize_place().getWidth())
+                .write("price",newPlace.getPrice())
+                .write("free", newPlace.isFree());
+        generator.writeEnd();
+        generator.close();
         System.out.println(newPlace.toString());
     }
 }
