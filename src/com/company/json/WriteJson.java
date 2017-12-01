b77887b56651ecaff8f9a5f1261509caa50482e1
manoelledeviron <manoelle.deviron@skynet.be>
2017-12-01 15:21:11 +0100

    changing json

--- src/com/company/json/WriteJson.java
+++ src/com/company/json/WriteJson.java
@@ -12,7 +12,7 @@ import javax.json.stream.JsonGenerator;
 
 public class WriteJson {
     public static void main(String[] args) throws IOException {
-        OutputStream fileoutputstream = new FileOutputStream(args[0]+".json");
+        OutputStream fileoutputstream = new FileOutputStream("Parking.json");
         JsonGenerator jsonGenerator = Json.createGenerator(fileoutputstream);
 
         Size newSize = new Size(200,300);
