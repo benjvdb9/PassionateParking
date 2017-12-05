1c131bda9e5861d1712ceb66051d4ac3e285ce9f
MisterTarock <victor_pb@hotmail.fr>
2017-12-05 13:17:50 +0100

    delete face

--- src/com/company/json/WriteJson.java
+++ src/com/company/json/WriteJson.java
@@ -7,13 +7,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import javax.json.Json;
-import javax.json.stream.JsonGenerator;
 
 public class WriteJson {
     public static void main(String[] args) throws IOException {
         OutputStream fileoutputstream = new FileOutputStream("Parking.json");
-        JsonGenerator jsonGenerator = Json.createGenerator(fileoutputstream);
+
 
         Size newSize = new Size(200,300);
         Place newPlace = new Place(newSize);
