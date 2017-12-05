e2d235cfbc88fea2459b5987c3a09ab2bbdf9a48
Paolo <paolodekeyzer@gmail.com>
2017-12-05 14:39:12 +0100

    tests comments

--- Tests/MyFirstTest.java
+++ Tests/MyFirstTest.java
@@ -11,35 +11,59 @@ import com.company.Color;
 public class MyFirstTest {
     @Test
     public void ParkingTest() {
+        //creation of a parking singleton
         Parking parking = Parking.getInstance();
+        //creation of a size object in order to have a parameter for the place
         Size size = new Size(1,2);
         Place place = new Place(size);
+        //test of length of the places array (array telling how many places are in total)
         Assert.assertEquals("should be 0",0, parking.getPlaces().size());
+        //adding a place to the list
         parking.addPlace(place);
+        //test the newly changed places array
         Assert.assertEquals("should be 1",1, parking.getPlaces().size());
+        //now testing the array listing how many busy and free places are in the parking
+
         Assert.assertEquals("should be 1",1, parking.getEmptyPlace().size());
         Assert.assertEquals("should be 1",0, parking.getBusyPlaces().size());
+        int totalsize = parking.getEmptyPlace().size()+parking.getBusyPlaces().size();
+        //test if sum of both arrays equals the size of the total places array
+        Assert.assertEquals("should be true",parking.getPlaces().size(), totalsize);
+        //Car creation in order to occupy a place
         Car car = new Car("ABC123");
         place.setBusy(car);
+        //retest the busy and free lists
         Assert.assertEquals("should be 1",0, parking.getEmptyPlace().size());
         Assert.assertEquals("should be 1",1, parking.getBusyPlaces().size());
+
+        totalsize = parking.getEmptyPlace().size()+parking.getBusyPlaces().size();
+        Assert.assertEquals("should be true",parking.getPlaces().size(), totalsize);
     }
     @Test
     public void SizeTest(){
+        //test to see if we can access size parameters
         Size size = new Size(1,2);
         Assert.assertEquals("should be 0",2, size.getLength());
         Assert.assertEquals("should be 0",1, size.getWidth());
     }
     @Test
     public void PlaceTest(){
+         //array of test for the Place class methods
+        //creation of the vatious objects needed for the place
         Size size = new Size(1,2);
         Place place = new Place(size);
         Car car = new Car("ABC123");
+        //Test if at the beginnig the place is free
         Assert.assertEquals("should be true", true,place.isFree());
+        //occupy the spot
         place.setBusy(car);
+        //test if the spot is not free anymore
         Assert.assertEquals("should be false", false,place.isFree());
+        //test which vehicle is stored on the place
         Assert.assertEquals("should work", car, place.getVehicle());
+        //test the place's price
         Assert.assertEquals( 0, place.getPrice(),0.001);
+
         Assert.assertEquals("should be false", false, place.isFree());
         place.setFree();
         Assert.assertEquals("should be true", true ,place.isFree());
