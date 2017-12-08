dd605a299378bb84ca968db636b38f26fcd342ce
jnshuti93 <jnshuti93@yahoo.com>
2017-12-08 17:00:59 +0100

    Test

--- src/test/testParking.java
+++ src/test/testParking.java
@@ -1,4 +1,23 @@
 package test;
 
+import org.junit.Test;
+
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class testParking {
+    public testParking() {
+    }
+
+    /**
+     * Test Constructor method for {@link manageleague.Team#Team(String)}.
+     */
+    @Test
+    public void testTeamConstruct() {
+        String name = "Real Madrid FC";
+        manageleague.Team team = new Team(name);
+
+        assertThat("Couldn't create team",team.getName(), is(name));
+    }
 }
